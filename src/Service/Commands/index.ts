861017521586cda4f7d8ad3172d3a3e30f8b34fa
Vrag404 <vrag404@protonmail.com>
2023-03-15 09:14:41 -0300

    Inserting new modules in commands

--- src/Service/Commands/index.ts
+++ src/Service/Commands/index.ts
@@ -1,8 +1,13 @@
 import { CommandTypes } from "./types";
 
-import { Sticker } from "../../Modules";
+import { Authorize, Menu, MyNumber, Readme, Revoke, Sticker, WarnEveryone } from "../../Modules";
 
 export const Commands: CommandTypes = {
   s: Sticker,
-  sticker: Sticker,
+  readme: Readme,
+  menu: Menu,
+  mynumber: MyNumber,
+  warnall: WarnEveryone,
+  authorize: Authorize,
+  revoke: Revoke,
 };

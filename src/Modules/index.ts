3f58764f88e81268583392279e96cd0e9c706f82
Vrag404 <vrag404@protonmail.com>
2023-03-15 09:12:19 -0300

    Adding new modules

--- src/Modules/index.ts
+++ src/Modules/index.ts
@@ -1,1 +1,8 @@
 export * from "./Sticker";
+export * from "./WarnAll";
+export * from "./Leave";
+export * from "./Menu";
+export * from "./MyNumber";
+export * from "./Readme";
+export { Authorize } from "./Authorization/authorizate";
+export { Revoke } from "./Authorization/revoke";
